86767157ed81a30c43c56eb8af26e051d51bcdbf
Kätlin Selberg <katlin.selberg@etsnord.ee>
2023-02-19 22:57:51 +0200

    add week 4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Random rnd = new Random();
+
+int myRandomNum = rnd.Next(0, 11); // 0-10
+
+int randomsum = 0;
+
+for (int i = 0; i < 10; i++) 
+{
+    Console.WriteLine($"My random number is: {myRandomNum}");
+    randomsum = randomsum + myRandomNum;
+}
+
+Console.WriteLine($"random sum total:{randomsum}");
+
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopactive = true; //bool - true/false
+while (loopactive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine($"cpu has generated {cpuRandom}.");
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+    int userNumber = Int32.Parse(Console.ReadLine());
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        //Loopactive = false;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Oops. Try again!");
+    }
+}
+Console.WriteLine("Have a nice day!");
